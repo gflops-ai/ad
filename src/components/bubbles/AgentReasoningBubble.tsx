767a488246fcd9eecdc92caf4320e7879c8a2e72
ryosuzuki30 <158655359+ryosuzuki30@users.noreply.github.com>
2024-06-30 08:42:06 +0900

    Update fontsize AgentReasoningBubble.tsx

--- src/components/bubbles/AgentReasoningBubble.tsx
+++ src/components/bubbles/AgentReasoningBubble.tsx
@@ -11,7 +11,7 @@ type Props = {
 
 const defaultBackgroundColor = '#f7f8ff';
 const defaultTextColor = '#303235';
-const defaultFontSize = 16;
+const defaultFontSize = 13;
 
 Marked.setOptions({ isNoP: true });
 
